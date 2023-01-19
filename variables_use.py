8c4af3648437d46b5b16a47a67c8b01d648a2d06
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-19 13:45:48 +0530

    Usage of variables.

---
+++ variables_use.py
@@ -0,0 +1,10 @@
+name = "Gopi Krishna"
+isStudent = False
+house_no = 223
+
+company = employer = petitioner = "Company"
+
+full_name, isEmployee, street_no = "Krishna", True, 555
+
+print(f"Company details: {company} , {employer} , {petitioner}")
+print(f"Details: {full_name} , {street_no} , {isEmployee}")
