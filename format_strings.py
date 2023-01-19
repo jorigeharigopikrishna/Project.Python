ee376c83cfec7cd726bcb00253c2fd5c25a18ef2
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-19 13:39:29 +0530

    String Representation.

--- format_strings.py
+++ format_strings.py
@@ -5,3 +5,12 @@ string_format_1 = f"Welcome to {language}"
 print(string_format_1)
 string_format_2 = "Welcome to {}".format(language)
 print(string_format_2)
+
+single_quote_string = 'Single Quotes String'
+double_quote_string = "Double Quotes String"
+triple_quote_string = """Triple Quotes String"""
+
+multi_line_string = """Multi Line String
+                    Line 2
+                    Line 3
+                    """
