f4dd1ccbc42bbc5cdcea6429210417cad239985f
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-19 13:33:26 +0530

    Various Ways of formatting strings.

---
+++ format_strings.py
@@ -0,0 +1,7 @@
+language = "Python"
+
+print("Welcome to ", language)
+string_format_1 = f"Welcome to {language}"
+print(string_format_1)
+string_format_2 = "Welcome to {}".format(language)
+print(string_format_2)
