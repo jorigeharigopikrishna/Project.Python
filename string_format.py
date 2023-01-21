3835bf75fad31cbe9df8f11fdcf7072d50dff990
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-21 21:56:44 +0530

    Format strings changes.

--- string_format.py
+++ string_format.py
@@ -14,3 +14,8 @@ string_format_2 = f"Welcome to {sample_string}"
 print(string_format_2)
 string_format_3 = "Welcome to {}".format(sample_string)
 print(string_format_3)
+
+num_1 = 24
+num_2 = 50
+string_format_4 = "Addition of %s and %s is %s" %(num_1, num_2, num_1 + num_2)
+print(string_format_4)
