d185297bc3b497e6932020f32b7c6d5e9aba8a9d
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-27 13:52:13 +0530

    use of lambdas.

---
+++ basics/use_of_lambdas.py
@@ -0,0 +1,8 @@
+square_of = lambda x: x * x
+print(square_of(4))
+
+add_numbers = lambda a, b: a + b
+print(add_numbers(1, 2))
+
+multiply_numbers = lambda a, b: a * b
+print(multiply_numbers(1, 2))
