4572bb6a2d73d38835191901fd7bd88080d33ea0
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-01-29 01:47:45 +0530

    custom module.

---
+++ custom_modules/calculator.py
@@ -0,0 +1,4 @@
+import calculator_functions as calc
+
+add_values = calc.add(1, 2)
+print(add_values)
---
+++ custom_modules/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="calculator",
+    version="1.0",
+    description="Calculator operations",
+    author="Gopi Krishna",
+    author_email="abcd.com",
+    url="abcdef.com",
+    py_modules=[
+        "calculator_functions"
+    ],
+)
